a901ba1c94010ce2747e11117db74bb2aebfedb7
Sai-Mannava <121317962+Sai-Mannava@users.noreply.github.com>
2023-11-23 20:36:39 -0800

    Update CoachViewController.swift

--- CoachViewController.swift
+++ CoachViewController.swift
@@ -2,22 +2,30 @@ import UIKit
 
 class CoachViewController: UIViewController, UITextViewDelegate {
 
+   
     
     @IBOutlet weak var inputLabel: UITextField!
     
     @IBOutlet weak var responseLabel: UITextView!
     
+    
+    @IBAction func askCoach(_ sender: UIButton) {
+        // Call OpenAI service to get the response
+        var prompt = "Break down today's workout concisely:  2x[6x(200+400)] and what pace should I go as my current mile time is \(mileText.text!)"
+        fetchOpenAIResponse(prompt: prompt)
+    }
+    
+    @IBOutlet weak var mileText: UITextField!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Prompt question: I want to 400s what pace should I go?
-        let prompt = "Break down today's workout:  2x[6x(200+400)]"
-
         // Assign UITextView delegate if needed
         responseLabel.delegate = self
 
-        // Call OpenAI service to get the response
-        fetchOpenAIResponse(prompt: prompt)
+        
+        
     }
 
     private func fetchOpenAIResponse(prompt: String) {
